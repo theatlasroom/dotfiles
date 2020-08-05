1d620a7bf88ba3a21ec162f69c27e2920e9cd197
Ezekiel <theatlasroom@gmail.com>
2020-08-06 01:07:17 +1000

    Minor update dotfiles

--- gitlab/path.zsh
+++ gitlab/path.zsh
@@ -1,10 +1,5 @@
 # Gitlab things
-export PATH="/usr/local/opt/postgresql@10/bin:/usr/local/opt/node@10/bin:$PATH"
+export PATH="/usr/local/opt/postgresql@11/bin:/usr/local/opt/node@12/bin:$PATH"
 
 # Fixes path issues for icu-i18n
 export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig:$PKG_CONFIG_PATH"
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,7 @@
 
 [[ -s "$HOME/.profile" ]] && source "$HOME/.profile" # Load the default .profile
 
-export PATH="/usr/local/opt/postgresql@10/bin:/usr/local/opt/node@10/bin:$PATH"
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+# RVM can encounter errors if it's not the last thing in .bash_profile
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to path for scripting (to manage Ruby versions)
+export PATH="$GEM_HOME/bin:$PATH"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*
