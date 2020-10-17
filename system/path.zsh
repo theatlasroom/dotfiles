a878dfbdd314068ae802e6b73003c490286fa998
Ezekiel <theatlasroom@gmail.com>
2020-10-17 21:32:56 +1100

    Minor update

--- system/path.zsh
+++ system/path.zsh
@@ -5,3 +5,6 @@
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to path for scripting (to manage Ruby versions)
 export PATH="$GEM_HOME/bin:$PATH"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*
+
+# Set path for openssl
+export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig:$PKG_CONFIG_PATH"
