a9a25aad8dce520a4bc2e43ee764188dad390fe0
Ezekiel <theatlasroom@gmail.com>
2020-10-19 13:17:42 +1100

    Remove rvm configurations

--- system/path.zsh
+++ system/path.zsh
@@ -1,10 +1,5 @@
 
 [[ -s "$HOME/.profile" ]] && source "$HOME/.profile" # Load the default .profile
 
-# RVM can encounter errors if it's not the last thing in .bash_profile
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to path for scripting (to manage Ruby versions)
-export PATH="$GEM_HOME/bin:$PATH"
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # Load RVM into a shell session *as a function*
-
 # Set path for openssl
 export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig:$PKG_CONFIG_PATH"
