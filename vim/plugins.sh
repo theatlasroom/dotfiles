893235220416910b15efc534099e98564787c61d
Ezekiel <theatlasroom@gmail.com>
2021-01-06 01:02:07 +1100

    Updated brewfile, vim and zprezto installation

--- vim/plugins.sh
+++
@@ -1,6 +0,0 @@
-# install vim plug
-# Add directory for installing plugins
-mkdir ~/.vim/autoload
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
