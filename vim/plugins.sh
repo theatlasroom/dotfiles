9188d4831ae5486e2acbe3be3d957f8d4d3bc7c7
Ezekiel <theatlasroom@gmail.com>
2021-01-05 22:55:18 +1100

    minor cleanup vim plugins

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -1,3 +1,6 @@
 # install vim plug
+# Add directory for installing plugins
+mkdir ~/.vim/autoload
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
