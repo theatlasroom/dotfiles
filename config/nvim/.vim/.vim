7706587943463fd69568ea38f1c9b40488afd5fb
Ezekiel <theatlasroom@gmail.com>
2021-01-06 21:29:40 +1100

    Minor update

---
+++ config/nvim/.vim/.vim
@@ -0,0 +1,1 @@
+/Users/ezekiel/.dotfiles/config/nvim/.vim
--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,4 +1,3 @@
-ZSH=${ZSH:-~/.oh-my-zsh}
-
-# install ohmyzsh - i prefer the aliases
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install ohmyzsh - i prefer the aliases to zprezto
+# we need to unset the ZSH path before running this, otherwise the install fails
+ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
