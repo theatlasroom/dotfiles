fccf84c60b7c986bc0231312fcb7312bc0ea4991
Ezekiel <theatlasroom@gmail.com>
2021-01-06 23:05:22 +1100

    cleanup

--- zprezto/install.sh
+++ zprezto/install.sh
@@ -1,14 +1,12 @@
 # Install zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+# mkdir "~/.zprezto/contrib"
 
 # # Copy default config files 
-# setopt EXTENDED_GLOB
-# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-#   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-# done
+sh ~/.dotfiles/zprezto/copy-deps.sh
 
 # clone zprezto-contrib
-git clone --recurse-submodules https://github.com/belak/prezto-contrib "${ZPREZTODIR}/contrib"
+git clone --recurse-submodules https://github.com/belak/prezto-contrib "${ZDOTDIR:-$HOME}/.zprezto/contrib"
 
 # Set Zsh as your default shell
 chsh -s /bin/zsh
