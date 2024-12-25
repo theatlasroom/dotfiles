376022842ed4ae86c046d4ed77d131dac3340500
Ezekiel <theatlasroom@gmail.com>
2024-12-25 21:35:49 +1100

    config update

--- gitlab/aliases.zsh
+++ gitlab/aliases.zsh
@@ -1,129 +1,133 @@
-# kill node
-alias killnode="killall -KILL node"
+# GitLab work related aliases
 
-# kill ruby
-alias killruby="killall -KILL ruby"
+if $GL_MODE; then
+	# kill node
+	alias killnode="killall -KILL node"
 
-# kill spring
-alias killspring="killall spring"
+	# kill ruby
+	alias killruby="killall -KILL ruby"
 
-# kill postgres 
-alias killpostgres="killall -KILL postgres"
+	# kill spring
+	alias killspring="killall spring"
 
-# kill workhorse, runner and pages
-alias killghrp="killall -KILL gitlab-workhorse gitlab-runner gitlab-pages"
+	# kill postgres 
+	alias killpostgres="killall -KILL postgres"
 
-# kill jaeger
-alias killjag="killall -KILL jaeger-all-in-one"
+	# kill workhorse, runner and pages
+	alias killghrp="killall -KILL gitlab-workhorse gitlab-runner gitlab-pages"
 
-# kill glab
-alias killglab="killnode && killruby && killpostgres && killjag && killghrp"
+	# kill jaeger
+	alias killjag="killall -KILL jaeger-all-in-one"
 
-# glab dirs
-alias foss="~/glab/gdk/gitlab-foss"
-alias ee="~/glab/gdk/gitlab"
-alias glee="ee"
-alias glui="~/glab/gitlab-ui"
+	# kill glab
+	alias killglab="killnode && killruby && killpostgres && killjag && killghrp"
 
-alias killgdk="ps aux | grep gitlab | awk '{print $2}' | xargs kill"
-alias wtfgitaly="ps aux | grep gitaly"
-alias gupdate="gdk update && gco db/schema.rb"
-# alias gupd="gupdate && gfresh"
-alias grdb="gdk run db"
-alias gapp="gdk run app"
-alias grun="gdk run"
-alias gstart="gdk start"
-alias grestart="gdk stop && gdk start"
-alias gstop="gdk stop"
-# Skip pipelines on push, not quite working 🤔
-alias gpnoci="git push -o ci.skip"
-alias gpflnoci="git push -o ci.skip --force-with-lease"
-# Force with lease, skip verify by default
-alias gpfl="git push --force-with-lease --no-verify"
-alias gpflverify="git push --force-with-lease"
-# Refresh gdk
-gdk-refresh() {
-		bundle exec rails db:migrate RAILS_ENV=development
-}
-alias gfresh="gdk-refresh"
-gffenableall(){
-  for i in "$@"
-  do
-	echo "Feature.enable(:$i)" | bundle exec rails c
-  done
-}
-gffdisableall(){
-  for i in "$@"
-  do
-	echo "Feature.disable(:$i)" | bundle exec rails c
-  done
-}
-gffenable(){
-  echo "Feature.enable(:$1)" | bundle exec rails c
-}
-gffdisable(){
-  echo "Feature.disable(:$1)" | bundle exec rails c
-}
-gfflist(){
-  echo "Feature.all.filter { |f| f.state == :on }.collect { |f| f.name }.sort{ |a,b| a <=> b }" | bundle exec rails c
-}
-# Enable / disable feature flags
-alias gffe="gffenable"
-alias gffd="gffdisable"
-alias gffl="gfflist"
-alias gffeall="gffenableall"
-alias gffdall="gffdisableall"
+	# glab dirs
+	alias foss="~/glab/gdk-foss/gitlab"
+	alias ee="~/glab/gdk/gitlab"
+	alias gl="ee"
+	alias glab="ee"
+	alias glui="~/glab/gitlab-ui"
 
-# Attempts to automatically update master and return to the current branch
-# Useful just before a rebase
-gupdate-master(){
-  # Store the current branch name
-  branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
-  branch_name="(unnamed branch)"     # detached HEAD
-  branch_name=${branch_name##refs/heads/}
+	alias killgdk="ps aux | grep gitlab | awk '{print $2}' | xargs kill"
+	alias wtfgitaly="ps aux | grep gitaly"
+	alias gupdate="gdk update && gco db/schema.rb"
+	# alias gupd="gupdate && gfresh"
+	alias grdb="gdk run db"
+	alias gapp="gdk run app"
+	alias grun="gdk run"
+	alias gstart="gdk start"
+	alias grestart="gdk stop && gdk start"
+	alias gstop="gdk stop"
+	# Skip pipelines on push, not quite working 🤔
+	alias gpnoci="git push -o ci.skip"
+	alias gpflnoci="git push -o ci.skip --force-with-lease"
+	# Force with lease, skip verify by default
+	alias gpfl="git push --force-with-lease --no-verify"
+	alias gpflverify="git push --force-with-lease"	
+	# Refresh gdk
+	gdk-refresh() {
+			yarn && bundle
+			bin/rails db:migrate RAILS_ENV=development
+	}
+	alias gfresh="gdk-refresh"
+	gffenableall(){
+		for i in "$@"
+		do
+		echo "Feature.enable(:$i)" | bundle exec rails c
+		done
+	}
+	gffdisableall(){
+		for i in "$@"
+		do
+		echo "Feature.disable(:$i)" | bundle exec rails c
+		done
+	}
+	gffenable(){
+		echo "Feature.enable(:$1)" | bundle exec rails c
+	}
+	gffdisable(){
+		echo "Feature.disable(:$1)" | bundle exec rails c
+	}
+	gfflist(){
+		echo "Feature.all.filter { |f| f.state == :on }.collect { |f| f.name }.sort{ |a,b| a <=> b }" | bundle exec rails c
+	}
+	# Enable / disable feature flags
+	alias gffe="gffenable"
+	alias gffd="gffdisable"
+	alias gffl="gfflist"
+	alias gffeall="gffenableall"
+	alias gffdall="gffdisableall"
 
-  echo "Currently on branch $branch_name"
+	# Attempts to automatically update master and return to the current branch
+	# Useful just before a rebase
+	gupdate-master(){
+	  # Store the current branch name
+	  branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+	  branch_name="(unnamed branch)"     # detached HEAD
+	  branch_name=${branch_name##refs/heads/}
 
-  # udpate gdk
-  gdk update
+	  echo "Currently on branch $branch_name"
 
-  echo "Restoring branch $branch_name"
+	  # udpate gdk
+	  gdk update
 
-  # restore original branch
-  git checkout "$branch_name"
+	  
+	  echo "Checking out the schema file"
 
-  echo "Refresh GDK"
+	  # remove the annoying changes in db/schema
+	  git checkout db
 
-  # refresh the gdk
-  gfresh
+	  echo "Restoring branch $branch_name"
 
-  echo "Checking out the schema file"
+	  # restore original branch
+	  git checkout "$branch_name"
 
-  # remove the annoying changes in db/schema
-  git checkout db  
-}
-alias gupd="gupdate-master"
+	  echo "Refresh GDK"
 
-# Checkout a tag gctag "v12.10-ee"
-gitcheckout-tag(){
-  TAG="$1"
+	  # refresh the gdk
+	  gfresh
+	}
+	alias gupd="gupdate-master"
 
-  git checkout tags/"$TAG" -b "$TAG"
-  
-  # ignore the db file
-  git checkout db
+	# Checkout a tag gctag "v12.10-ee"
+	gitcheckout-tag(){
+	  TAG="$1"
 
-  # refresh gdk and assets
-  yarn && bundle exec rails db:migrate RAILS_ENV=development
-  gdk reconfigure
-  gdk restart
-}
-alias gctag="gitcheckout-tag"
-alias glpreparemr="bin/rake gettext:regenerate && yarn lint:eslint:all:fix && yarn lint:prettier:fix"
-alias glspiritbomb=glpreparemr
+	  git checkout tags/"$TAG" -b "$TAG"
+	  
+	  # ignore the db file
+	  git checkout db
 
-alias secee="ee && ./scripts/security-harness"
-alias glsec="secee"
+	  # refresh gdk and assets
+	  yarn && bin/rake db:migrate RAILS_ENV=development
+	  gdk reconfigure
+	  gdk restart
+	}
+	alias gctag="gitcheckout-tag"
+	alias preparemr="bin/rake gettext:regenerate && yarn lint:eslint:all:fix && yarn lint:prettier:fix"
+	alias spiritbomb=preparemr
 
-alias glshallowupdate="yarn && bundle install && bundle exec rails db:migrate"
-alias glsu=glshallowupdate
+	alias secee="ee && ./scripts/security-harness"
+	alias glsec="secee"
+fi
--- gitlab/path.zsh
+++ gitlab/path.zsh
@@ -1,8 +1,13 @@
-# Gitlab things
-export PATH="${HOMEBREW_PATH}/postgresql@11/bin:${HOMEBREW_PATH}/node@12/bin:$PATH"
+# Only include these paths when using GL mode
 
-# Fixes path issues for icu-i18n
-export PKG_CONFIG_PATH="${HOMEBREW_PATH}/icu4c/lib/pkgconfig:$PKG_CONFIG_PATH"
+if $GL_MODE; then 
+	echo "GL MODE - setting path vars"
+	# Gitlab things
+	export PATH="/usr/local/opt/postgresql@11/bin:/usr/local/opt/node@12/bin:$PATH"
 
-# source bash completions for gitlab
-# source ~/glab/gdk/support/completions/gdk.bash
+	# Fixes path issues for icu-i18n
+	export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig:$PKG_CONFIG_PATH"
+
+	# source bash completions for gitlab
+	source ~/glab/gdk/support/completions/gdk.bash
+fi
